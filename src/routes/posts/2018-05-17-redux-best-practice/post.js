a84729309270490534a81fd4bc08e73f5e3eb94b
qingtong <qingtong@wacai.com>
2020-03-07 21:35:52 +0800

    add react&&redux

---
+++ src/routes/posts/2018-05-17-redux-best-practice/post.js
@@ -0,0 +1,5 @@
+export default {
+  title: `redux 最佳实践`,
+  spoiler: "redux 最佳实践，你想知道的都在这里",
+  getContent: () => import('./document.mdx'),
+}
---
+++ src/routes/posts/2018-08-15-you-dont-know-react/post.js
@@ -0,0 +1,5 @@
+export default {
+  title: `React 你不知道的那些事儿`,
+  spoiler: "关于 React 组件、引用和路由，以及更多",
+  getContent: () => import('./document.mdx'),
+}
